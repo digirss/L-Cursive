08f1a7ae8509c8405477396b926b4623fff82b96
Leon Li <digirss@gmail.com>
2026-01-16 03:39:11 +0800

    config: revert base path for production deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   return {
-    base: '/l-cursive/',
+    base: '/test/l-cursive/',
     server: {
       port: 3000,
       host: '0.0.0.0',
